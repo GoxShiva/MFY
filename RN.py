a20250933dddffd90b99a4f26b35235ee79a6d92
GoxShiva <151367976+GoxShiva@users.noreply.github.com>
2023-11-18 16:02:35 +0530

    Add files via upload

---
+++ RN.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+import os,platform
+
+# Define a flag or use a file to track whether the update has been done
+update_flag = "update_done.txt"
+
+if not os.path.exists(update_flag):
+os.system('git pull')
+ 
+    print('Sorry 32 Bit Not Supported...')
+elif MAFIA=="64bit":
+     __import__("Mafia")
